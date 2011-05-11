e7f313722d5f712b46e7c04344be99dde4f6c7cc
David Coss (hostname: turing) <David.Coss@stjude.org>
2011-05-11 08:42:00 -0500

    improved help function.

--- src/symrec.h
+++ src/symrec.h
@@ -7,34 +7,34 @@
 
 struct calcval{double re,im;};
 /* Function type.  */
-     typedef double (*func_t) (double);
+typedef double (*func_t) (double);
      
-     /* Data type for links in the chain of symbols.  */
-     struct symrec
-     {
-       typedef struct symrec* (*plane_func_t) (struct symrec **vars,size_t size);
+/* Data type for links in the chain of symbols.  */
+struct symrec
+{
+  typedef struct symrec* (*plane_func_t) (struct symrec **vars,size_t size);
 
-       char *name;  /* name of symbol */
-       int type;    /* type of symbol: either VAR or FNCT */
-       union
-       {
-         double var[2];      /* value of a VAR */
-         func_t fnctptr;  /* value of a FNCT */
-	 void (*v_c_fnctptr)(const char*);
-	 Plane<math::Complex>* planeptr;
-       } value;
-       plane_func_t plane_fnctptr;
-       bool isPlane;
-       struct symrec *next;  /* link field */
-     };
+  char *name;  /* name of symbol */
+  int type;    /* type of symbol: either VAR or FNCT */
+  union
+  {
+    double var[2];      /* value of a VAR */
+    func_t fnctptr;  /* value of a FNCT */
+    void (*v_c_fnctptr)(const char*);
+    Plane<math::Complex>* planeptr;
+  } value;
+  plane_func_t plane_fnctptr;
+  bool isPlane;
+  struct symrec *next;  /* link field */
+};
      
-     typedef struct symrec symrec;
+typedef struct symrec symrec;
      
-     /* The symbol table: a chain of `struct symrec'.  */
-     extern symrec *sym_table;
+/* The symbol table: a chain of `struct symrec'.  */
+extern symrec *sym_table;
      
-     symrec *putsym (char const *, int);
-     symrec *getsym (char const *);
+symrec *putsym (char const *, int);
+symrec *getsym (char const *);
 
 #endif
 
